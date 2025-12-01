ea4c24d13149f8ae0f25160adbe91ff54e948939
sama-eng <morshedysama@gmail.com>
2025-12-01 02:12:35 +0200

    image info

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+
 def file_exists(path):
     """Return True if the file exists, else False."""
     try:
@@ -12,3 +13,34 @@ def get_file_extension(path):
     if '.' in path:
         return path.rsplit('.', 1)[-1]
     return ''
+
+def read_image_size(path):
+    """Reads width and height from a PPM (P3) image file.
+    Returns (width, height)"""
+
+    try:
+        with open(path, 'r') as f:
+            lines = f.readlines()
+
+        content = [line.strip() for line in lines if line.strip() and not line.startswith('#')]
+
+        if content[0] != 'P3':
+            raise ValueError("Unsupported PPM format. Only P3 supported.")
+
+        dimensions = content[1].split()
+        width = int(dimensions[0])
+        height = int(dimensions[1])
+        return width, height
+
+    except Exception as e:
+        print("Error reading image size:", e)
+        return 0, 0
+    
+def calc_max_steg_size(width, height):
+    """
+    Calculates maximum number of characters that can be hidden in the image
+    using LSB logic (1 character = 8 bits, 3 channels RGB per pixel).
+    """
+    if width == 0 or height == 0:
+        return 0
+    return (width * height * 3) // 8
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from helper import (file_exists, get_file_extention)
+from helper import (file_exists, get_file_extension, read_image_size, calc_max_steg_size)
 
 
 def main():
@@ -6,7 +6,7 @@ def main():
     print("   Welcome to image steganography tool :) ")
     print("==================================\n")
 
-    print("Please provide  the path to your image (PPM format ONLY).")
+    print("Please provide the path to your image (PPM format ONLY).")
 
     while True: 
        image_path = input("Enter or drag the path to your PPM image: ").strip()
@@ -15,9 +15,19 @@ def main():
            print("ERROR: File not found. Please try again. \n")
            continue
        
-       extension = get_file_extention(image_path).lower()
+       extension = get_file_extension(image_path).lower()
        if extension != "ppm":
            print("ERROR: Unsupported format. Only PPM are allowed. \n")
            continue
        
        break
+
+    width,height = read_image_size(image_path)
+    max_chars = calc_max_steg_size(width, height)
+
+    print("\n-------------------------------")
+    print("Image Loaded Successfully!")
+    print("File:", image_path)
+    print(f"Size: {width} x {height}")
+    print(f"Max Steganography Size {max_chars}")
+    print("-------------------------------\n")
