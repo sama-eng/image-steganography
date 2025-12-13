48d35a6a47f0542ddd0c8b090c5ac90d8d22a7aa
sama-eng <morshedysama@gmail.com>
2025-12-13 22:18:33 +0200

    finalizing main - adding options so the user could choose what he needs (hide, extract, change the image, finally exit the whole program)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print("==================================\n")
 
     print("Please provide the path to your image (PPM format ONLY).")
+    print(" OR drag and drop the PPM file \n")
 
     while True: 
        image_path = input("Enter or drag the path to your PPM image: ").strip()
@@ -17,6 +18,7 @@ def main():
        extension = get_file_extension(image_path).lower()
        if extension != "ppm":
            print("ERROR: Unsupported format. Only PPM are allowed. \n")
+           print("Please convert your image to PPM format and try again.")
            continue
        
        break
@@ -24,9 +26,42 @@ def main():
     width,height = read_image_size(image_path)
     max_chars = calc_max_steg_size(width, height)
 
-    print("\n-------------------------------")
     print("Image Loaded Successfully!")
     print("File:", image_path)
     print(f"Size: {width} x {height}")
     print(f"Max Steganography Size {max_chars}")
-    print("-------------------------------\n")
+
+    while True:
+        print("Choose an option:")
+        print("1. Hide a message")
+        print("2. Extract a message")
+        print("3. Change image")
+        print("4. Exit program")
+        choice = input("Enter 1, 2, 3, or 4: ").strip()
+    
+        if choice == "1":
+            encode_message(image_path)  
+        elif choice == "2":
+            decode_message(image_path)
+        elif choice == "3":
+            while True:
+                image_path = input("Enter or drag the path to your new PPM image: ").strip()
+                if not file_exists(image_path):
+                    print("ERROR: File not found. Please try again.\n")
+                    continue
+                extension = get_file_extension(image_path).lower()
+                if extension != "ppm":
+                    print("ERROR: Only PPM images allowed. Convert and try again.\n")
+                    continue
+                width, height = read_image_size(image_path)
+                max_chars = calc_max_steg_size(width, height)
+                print("\nNew Image Loaded Successfully!")
+                print("File:", image_path)
+                print(f"Size: {width} x {height}")
+                print(f"Max Steganography Size: {max_chars}\n")
+                break
+        elif choice == "4":
+            print("\nProgram finished.")
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, 3, or 4.\n")
